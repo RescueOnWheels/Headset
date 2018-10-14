3700fb41edcabf6b52adecfb16a1f452483a7da7
Martijn Vegter <mail@mvegter.me>
2018-10-14 13:08:02 +0200

    Limit screen rotation and send rotation to Larynx

---
+++ Headset/Assets/Scripts/RotationLimit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLimit : MonoBehaviour {
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion target = camera.transform.rotation;
+
+		// Limit the X-axis between -45 and 45.
+		var x = target.eulerAngles.x;
+		if(target.eulerAngles.x < 180) {
+			x = Mathf.Clamp(x, 0, 45);
+		}
+		else {
+			x = Mathf.Clamp(x, 315, 360);
+		}
+		
+		var y = target.eulerAngles.y;
+		if(y < 180) {
+			y = Mathf.Clamp(y, 0, 45);
+		}
+		else {
+			y = Mathf.Clamp(y, 315, 360);
+		}
+
+		// Disable rotation on the Z-axis
+		target.eulerAngles = new Vector3(
+			x,
+			y, 
+			0
+		);
+
+		transform.rotation = target;
+	}
+}
--- Headset/Assets/Scripts/Socket.cs
+++ Headset/Assets/Scripts/Socket.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Quobject.SocketIoClientDotNet.Client;
 using UnityEngine;
 
@@ -18,7 +20,17 @@ public class Socket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		var x = Math.Round(gameObject.transform.localEulerAngles.x, 2);
+		var y = Math.Round(gameObject.transform.localEulerAngles.y, 2);
+		var z = Math.Round(gameObject.transform.localEulerAngles.z, 2);
+
+		var data = new {
+			x,
+			y,
+			z
+		};
+
+		socket.Emit("headset", JsonConvert.SerializeObject(data)	);
 	}
 
 	void OnDestroy() {
