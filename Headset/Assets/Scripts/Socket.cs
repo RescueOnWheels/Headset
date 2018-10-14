e730872e1f9a5b994f426bcce65bab7aef40f3e0
Martijn Vegter <mail@mvegter.me>
2018-10-14 15:36:52 +0200

    Updated data for headset position.

--- Headset/Assets/Scripts/Socket.cs
+++ Headset/Assets/Scripts/Socket.cs
@@ -7,30 +7,53 @@ using UnityEngine;
 
 public class Socket : MonoBehaviour {
 	Quobject.SocketIoClientDotNet.Client.Socket socket = null;
+	
 	// Use this for initialization
 	void Start () {
 		socket = IO.Socket("http://192.168.1.6:3000");
 
 		socket.On("connect", () =>
 		{
-			socket.Emit("hi");
-			
+			Debug.Log("Connected to Larynx.");
 		});
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var x = Math.Round(gameObject.transform.localEulerAngles.x, 2);
-		var y = Math.Round(gameObject.transform.localEulerAngles.y, 2);
-		var z = Math.Round(gameObject.transform.localEulerAngles.z, 2);
+		double x = gameObject.transform.eulerAngles.x;
+		double y = gameObject.transform.eulerAngles.y;
+		double z = gameObject.transform.eulerAngles.z;
+
+		double vertical = x;
+		if(x < 180) {
+			vertical = (-1 * (x / 45));
+		} else {
+			vertical = (1-((x-315) / 45));
+		}
+
+		vertical = Math.Round(vertical, 2);
+		vertical = Math.Min(vertical, 1.00);
+		vertical = Math.Max(vertical, -1.00);
+
+		double horizontal = y;
+		if(y < 180) {
+			horizontal = (y / 45);
+		} else {
+			horizontal = (-1 - (-1 * ((y-315) / 45)));
+		}
+
+		horizontal = Math.Round(horizontal, 2);
+		horizontal = Math.Min(horizontal, 1.00);
+		horizontal = Math.Max(horizontal, -1.00);
 
 		var data = new {
-			x,
-			y,
-			z
+			horizontal,
+			vertical
 		};
 
-		socket.Emit("headset", JsonConvert.SerializeObject(data)	);
+		Debug.Log(data);
+
+		socket.Emit("headset position", JsonConvert.SerializeObject(data));
 	}
 
 	void OnDestroy() {
